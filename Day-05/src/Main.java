986c4b5442f782ac07f0691266a00bb3d1e1a907
RML-Cambo <ltomusic69@gmail.com>
2024-12-15 18:33:09 +0700

    Day-05

--- Day-05/src/Main.java
+++ Day-05/src/Main.java
@@ -17,7 +17,29 @@ class Student {
 public class Main {
     public static void main(String[] args) {
 
-        // Create Object 
+        // Create Object
+        Student student1 = new Student();
+        student1.name = "Toch Ratana";
+        student1.age = 23;
+        student1.sex = 'M';
+
+        Student student2 = new Student();
+        student2.name = "Kong Dara";
+        student2.age = 19;
+        student2.sex = 'M';
+
+
+        // Access and display data members
+        student1.displayInfo();
+        System.out.println("-".repeat(20));
+        student2.displayInfo();
+
+        // Access static variable directly
+        System.out.println("-".repeat(20));
+        System.out.println("School for all Student : " + Student.school);
+
+
+
 
     }
 }
