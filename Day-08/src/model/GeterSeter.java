886aac684532eea21e3f4e286b4f844de64de748
RML-Cambo <ltomusic69@gmail.com>
2024-12-19 15:29:50 +0700

    Car

--- Day-08/src/model/GeterSeter.java
+++ Day-08/src/model/GeterSeter.java
@@ -1,21 +1,6 @@
 package model;
 
 public class GeterSeter {
-    String name;
-    String email;
-    Integer password;
-
-    void getder(String email){
-        this.email = email;
-    }
-    void setder(String email){
-        this.email = email;
-    }
-
-
-    public static void main(String[] args) {
-        GeterSeter geterSeter = new GeterSeter();
-
-    }
+    private Integer id = 10;
 
 }
