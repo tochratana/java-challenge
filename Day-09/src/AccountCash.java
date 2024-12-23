8315ea24f83cfbb7ac2cd3e4362114ff820ccc99
RML-Cambo <ltomusic69@gmail.com>
2024-12-23 08:38:32 +0700

    Record Keyword

---
+++ Day-09/src/AccountCash.java
@@ -0,0 +1,13 @@
+class Account {
+    Double amountCash = 0D;
+}
+
+
+public class AccountCash {
+    public static void main(String[] args) {
+        Account account = new Account();
+        System.out.println(account.amountCash);
+
+    }
+
+}
