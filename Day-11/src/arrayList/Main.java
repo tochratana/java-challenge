ee5981f481c37a05278027999dccfad7fbc0be0e
RML-Cambo <ltomusic69@gmail.com>
2024-12-26 23:48:02 +0700

    Day-11

---
+++ Day-11/src/arrayList/Main.java
@@ -0,0 +1,30 @@
+package arrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+class Student {
+
+    String name;
+    String email;
+    Integer id;
+
+    Student(String name, String email, Integer id){
+        this.name = name ;
+        this.email = email;
+        this.id = id;
+    }
+
+}
+public class Main {
+    public static void main(String[] args) {
+
+        List<Student> ratana = new ArrayList<>();
+        
+
+
+
+    }
+}
