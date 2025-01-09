99cdc970ea28f762dbfd2c0f12bbdfebc750f8ea
RML-Cambo <ltomusic69@gmail.com>
2025-01-09 08:26:40 +0700

    main

--- Day-12-OOP/src/libraries/Main.java
+++ Day-12-OOP/src/libraries/Main.java
@@ -5,18 +5,19 @@ public class Main {
 
         Library library = new Library();
 
-        System.out.println("\nAdding Books...");
+        System.out.println("\n[+] Adding Books");
         library.addBook(new Book(1, "The Great Gatsby", "F. Scott Fitzgerald"));
         library.addBook(new Book(2, "To Kill a Mockingbird", "Harper Lee"));
+        library.addBook(new Book(3, "KOKO Cher Pheng", "Kim Chansopheng"));
 
-        System.out.println("\nAdding Members...");
+        System.out.println("\n[+] Adding Members");
         library.registerMember(new Member(1, "Alice"));
         library.registerMember(new Member(2, "Bob"));
-
-        System.out.println("\nDisplaying All Books:");
+        // Book before borrow
+        System.out.println("\n" +"=".repeat(10) +" Displaying All Books "+"=".repeat(10));
         library.displayBooks();
 
-        System.out.println("\nDisplaying All Members:");
+        System.out.println("\n" +"=".repeat(10) +" Displaying All Members "+"=".repeat(10));
         library.displayMembers();
 
         Member bob = library.findMemberById(2);
@@ -24,21 +25,27 @@ public class Main {
         if (bob != null && greatGatsby != null) {
             bob.borrowBook(greatGatsby);
         }
+//
+//        Member bob = library.findMemberById(2);
+//        Book greatGatsby = library.findBookById(1);
+//        if (bob != null && greatGatsby != null) {
+//            bob.borrowBook(greatGatsby);
+//        }
 
-        System.out.println("\nDisplaying All Books:");
+        System.out.println("\n" +"=".repeat(10) +" Displaying All Books "+"=".repeat(10));
         library.displayBooks();
 
-        System.out.println("\nDisplaying All Members:");
+        System.out.println("\n" +"=".repeat(10) +" Displaying All Members "+"=".repeat(10));
         library.displayMembers();
-        
+
         if (bob != null && greatGatsby != null) {
             bob.returnBook(greatGatsby);
         }
 
-        System.out.println("\nDisplaying All Books:");
+        System.out.println("\n" +"=".repeat(10) +" Displaying All Books "+"=".repeat(10));
         library.displayBooks();
 
-        System.out.println("\nDisplaying All Members:");
+        System.out.println("\n" +"=".repeat(10) +" Displaying All Members "+"=".repeat(10));
         library.displayMembers();
     }
 }
